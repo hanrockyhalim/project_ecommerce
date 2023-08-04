3ef99a5197c710781b1ed8b72ad4f6e82f94cc9a
Hanrocky <hanrocky.halim@gmail.com>
2023-08-04 21:16:39 +0700

    Added product card

---
+++ my-app/src/lib/productCard.svelte
@@ -0,0 +1,11 @@
+<div class="card">
+    <header class="card-header">
+        <h2>
+            Product Name
+        </h2>
+    </header>
+    <div class="card-body px-4">Quantity: <strong>4 </strong></div>
+    <div class="card-body px-4">
+        Price: $10
+    </div>
+</div>
