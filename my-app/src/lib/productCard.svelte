089ba19509cf37f4da2ff9ba7d93cf9ca93ed989
Hanrocky <hanrocky.halim@gmail.com>
2023-08-16 21:06:20 +0700

    Add cart logic to product card

--- my-app/src/lib/productCard.svelte
+++ my-app/src/lib/productCard.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+    import { cartItems, addToCart, removeFromCart } from "../cart";
+    import { get } from "svelte/store";
     export let product : Product = {
         id : '', name:'', price:0
     }
+    let cart = get(cartItems)
+    let cartItemIndex = cart.findIndex((item) => {return item.id === product.id})
+    let cartProduct = cart[cartItemIndex];
+    cartItems.subscribe((newCartValue) => {
+        cart = newCartValue;
+        cartItemIndex = cart.findIndex((item) => {return item.id === product.id})
+        cartProduct = cart[cartItemIndex];
+    })
+
     console.log(product)
 </script>
 
@@ -12,12 +23,17 @@
             {product.name}
         </h1>
     </header>
-    <div class="card-body px-4">Quantity: <strong>4 </strong></div>
+    {#if cartProduct !== undefined}
+    <div class="card-body px-4">
+        
+        Quantity: <strong>{cartProduct.quantity} </strong>
+    </div>
+    {/if}
     <div class="card-body px-4">
         Price: ${product.price}
     </div>
     <footer class="card-footer">
-        <button class="p-2 rounded variant-glass-primary" >Add</button>
-        <button class="p-2 rounded variant-glass-error" >Remove</button>
+        <button class="p-2 rounded variant-glass-primary" on:click={() => addToCart(product.id)} >Add</button>
+        <button class="p-2 rounded variant-glass-error" on:click={() => removeFromCart(product.id)} >Remove</button>
     </footer>
 </div>
